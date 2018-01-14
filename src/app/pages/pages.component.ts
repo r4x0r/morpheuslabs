68fa4b7d24ae9e1216ea3b3a3ca7a67f6f863a43
Xu Cong <xc4330@gmail.com>
2018-01-14 23:04:23 +0800

    Added user name and email

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-
 import { NbMenuItem } from '@nebular/theme';
+import { KeycloakService } from '../shared/keycloak/keycloak.service';
+import { UserService } from '../services/user.service'
+import { User } from '../model/user.model'
 
 @Component({
   selector: 'ml-pages',
@@ -65,4 +67,27 @@ export class PagesComponent {
       link: '/pages/membership'
     }
   ]
+
+  kcUserProfile: any = {};
+  isUserLoaded: boolean = false;
+  constructor(
+    private kc: KeycloakService,
+    private us: UserService
+  ) { }
+
+  ngOnInit() {
+    this.kc.loadProfile().then(user => {
+      this.kcUserProfile = user;
+      // console.log(this.kcUserProfile)
+      this.kc.getToken().then(res => {
+        // console.log(res)
+        let user = this.us.initUser(
+          new User(res,this.kcUserProfile.firstName,this.kcUserProfile.lastName,this.kcUserProfile.username,this.kcUserProfile.email)
+        )
+        this.isUserLoaded = true
+        console.log(this.us.getUser())
+      })
+		})
+  }
+
 }
--- src/app/pages/pagescomponents/mlsidebar/profilepic/profilepic.component.ts
+++ src/app/pages/pagescomponents/mlsidebar/profilepic/profilepic.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../../services/user.service'
+import { User } from '../../../../model/user.model'
 
 @Component({
   selector: 'ml-profilepic',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilepicComponent implements OnInit {
 
-  constructor() { }
+  user: User
+  constructor(private us: UserService) { }
 
   ngOnInit() {
+    this.user = this.us.getUser()
+    console.log(this.user)
   }
 
 }
