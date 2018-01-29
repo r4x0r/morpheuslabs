5b9f97ff65e26f311fb3c603a9f5e1b39c6aee24
Xu Cong <xc4330@gmail.com>
2018-01-29 10:47:25 +0800

    Changed prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  realm: 'che',
+  clientId: 'che-public',
+  keycloakBaseUrl: 'http://bpaas.morpheuslabs.io:5050/auth',
+  serviceBaseUrl: 'http://bpaas.morpheuslabs.io/api'
 };
